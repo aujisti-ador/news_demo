a04555837073878ec47b6c5a152efdfa1863d3d9
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2018-01-03 13:39:05 +0600

    delete modified

--- delete.php
+++ delete.php
@@ -20,3 +20,20 @@ function delete_news($news_id) {
         die('Sql Problem' . mysql_error());
     }
 }
+
+function delete_news1($news_id) {
+    $db_connect = mysqli_connect('localhost', 'root', '');
+    if ($db_connect) {
+        mysqli_select_db($db_connect, 'db_practice');
+    } else {
+        die('Database Problem!' . mysql_error());
+    }
+
+    $sql = "DELETE FROM tbl_news WHERE news_id='$news_id' ";
+
+    if (mysqli_query($db_connect, $sql)) {
+        header('Location:view_news.php');
+    } else {
+        die('Sql Problem' . mysql_error());
+    }
+}
